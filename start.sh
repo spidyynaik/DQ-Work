4a54836cc42622ac027b6f28bb9729aa19e8bb68
spidyynaik <128451864+spidyynaik@users.noreply.github.com>
2023-06-03 16:54:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/azanhelpdesk/DQ-_TOM.git /DQ-_TOM
+  git clone https://github.com/spidyynaik/DQ-Work.git /DQ-_TOM
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-_TOM
